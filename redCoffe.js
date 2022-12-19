6b0ab96754bc9f757e515fd44fce23311d6fd3ff
YerkanatSultanov <erkanatsultanov1999@gmail.com>
2022-12-20 02:23:28 +0600

    js redCoffee

--- redCoffe.js
+++ redCoffe.js
@@ -1,87 +1,87 @@
 const arr = [
     {
         html: '            <div class="samsaSKurPhoto"><img src="./img/png-clipart-coffee-marocchino-latte-cafe-au-lait-espresso-coffee-cafe-teacup-removebg-preview.png" alt="samsa" class="photo" ></div>\n' +
-            '            <div class="text">Американо</div>\n' +
+            '            <div class="text" id="text1" >Американо</div>\n' +
             '            <div class="priceAndCart">\n' +
-            '                <div class="price">600 Тг</div>\n' +
+            '                <div class="price" id="price1">600</div>\n' +
             '                <div class="cart">\n' +
-            '                    <button class="toCart">В корзину</button>\n' +
+            '                    <button class="toCart"  onclick="insert(1)">В корзину</button>\n' +
             '                </div>\n' +
             '            </div>',
         type: 'Кофе'
     },
     {
         html: '            <div class="samsaSKurPhoto"><img src="./img/Espresso-PNG-Free-Download.png" alt="samsa" class="photo"></div>\n' +
-            '            <div class="text">Экспрессо</div>\n' +
+            '            <div class="text" id="text2">Экспрессо</div>\n' +
             '            <div class="priceAndCart">\n' +
-            '                <div class="price">650 Тг</div>\n' +
+            '                <div class="price" id="price2">650</div>\n' +
             '                <div class="cart">\n' +
-            '                    <button class="toCart">В корзину</button>\n' +
+            '                    <button class="toCart" onclick="insert(2)">В корзину</button>\n' +
             '                </div>\n' +
             '            </div>',
         type: 'Кофе'
     },
     {
         html: '<div class="samsaSKurPhoto"><img src="./img/png-transparent-caffe-macchiato-latte-macchiato-cafe-au-lait-cappuccino-coffee-glass-cafe-coffee-removebg-preview.png" alt="samsa" class="photo" style="width: 200px; height: 150px"></div>\n' +
-            '            <div class="text">Латте</div>\n' +
+            '            <div class="text" id="text3">Латте</div>\n' +
             '            <div class="priceAndCart">\n' +
-            '                <div class="price">600 Тг</div>\n' +
+            '                <div class="price" id="price3">600</div>\n' +
             '                <div class="cart">\n' +
-            '                    <button class="toCart">В корзину</button>\n' +
+            '                    <button class="toCart" onclick="insert(3)">В корзину</button>\n' +
             '                </div',
         type: 'Кофе'
     },
     {
         html: '<div class="samsaSKurPhoto"><img src="./img/5251578363822-removebg-preview.png" alt="samsa" class="photo"></div>\n' +
-            '            <div class="text">Моккачино</div>\n' +
+            '            <div class="text" id="text4">Моккачино</div>\n' +
             '            <div class="priceAndCart">\n' +
-            '                <div class="price">250 Тг</div>\n' +
+            '                <div class="price" id="price4">250</div>\n' +
             '                <div class="cart">\n' +
-            '                    <button class="toCart">В корзину</button>\n' +
+            '                    <button class="toCart" onclick="insert(4)">В корзину</button>\n' +
             '                </div>\n' +
             '            </div>',
         type: 'Кофе'
     },
     {
         html: '            <div class="samsaSKurPhoto"><img src="./img/26-264924_chocolate-frapp-frapp-coffee-removebg-preview.png" alt="samsa" class="photo" style="width: 250px; height: 150px"></div>\n' +
-            '            <div class="text">Фраппе</div>\n' +
+            '            <div class="text" id="text1">Фраппе</div>\n' +
             '            <div class="priceAndCart">\n' +
-            '                <div class="price">700 Тг</div>\n' +
+            '                <div class="price" id="price5">700</div>\n' +
             '                <div class="cart">\n' +
-            '                    <button class="toCart">В корзину</button>\n' +
+            '                    <button class="toCart" onclick="insert(5)">В корзину</button>\n' +
             '                </div>\n' +
             '            </div>',
         type: 'Кофе'
     },
     {
         html: '            <div class="samsaSKurPhoto"><img src="./img/Kruassan-s-kuritsey-gril-i-zharenym-bekonom-removebg-preview.png" alt="samsa" class="photo"></div>\n' +
-            '            <div class="text">Круассан с курицей</div>\n' +
+            '            <div class="text" id="text5">Круассан с курицей</div>\n' +
             '            <div class="priceAndCart">\n' +
-            '                <div class="price">450 Тг</div>\n' +
+            '                <div class="price" id="price6">450</div>\n' +
             '                <div class="cart">\n' +
-            '                    <button class="toCart">В корзину</button>\n' +
+            '                    <button class="toCart" onclick="insert(6)">В корзину</button>\n' +
             '                </div>\n' +
             '            </div>',
         type: 'Фаст-фуд',
     },
     {
         html: '            <div class="samsaSKurPhoto"><img src="./img/34-349922_sandwiches--removebg-preview.png" alt="samsa" class="photo"></div>\n' +
-            '            <div class="text">Клаб-Сендвич с курицей</div>\n' +
+            '            <div class="text" id="text6">Клаб-Сендвич с курицей</div>\n' +
             '            <div class="priceAndCart">\n' +
-            '                <div class="price">400 Тг</div>\n' +
+            '                <div class="price" id="price7">400</div>\n' +
             '                <div class="cart">\n' +
-            '                    <button class="toCart">В корзину</button>\n' +
+            '                    <button class="toCart" onclick="insert(7)">В корзину</button>\n' +
             '                </div>\n' +
             '            </div>',
         type: 'Фаст-фуд'
     },
     {
         html: '            <div class="samsaSKurPhoto"><img src="./img/png-transparent-chocolate-donut-with-sprinkles-illustration-doughnut-dessert-bakery-cake-donut-collection-food-cake-decorating-sweetness-removebg-preview.png" alt="samsa" class="photo" style="width: 180px"></div>\n' +
-            '            <div class="text">Пончик</div>\n' +
+            '            <div class="text" id="text7">Пончик</div>\n' +
             '            <div class="priceAndCart">\n' +
-            '                <div class="price">150 Тг</div>\n' +
+            '                <div class="price" id="price1">150 Тг</div>\n' +
             '                <div class="cart">\n' +
-            '                    <button class="toCart">В корзину</button>\n' +
+            '                    <button class="toCart" onclick="insert(1)">В корзину</button>\n' +
             '                </div>\n' +
             '            </div>',
         type: 'Фаст-фуд'
@@ -111,4 +111,22 @@ function clickk2(element) {
     for (let i = 0; i < fill.length; i++) {
         content.innerHTML += `<div class = "Part" id = "${i}"> ${fill[i].html} </div>`;
     }
+}
+
+function insert(a) {
+    let j = '#text' + a;
+    let l = '#price' + a;
+    let name=$(j).text();
+    let price = $(l).text();
+    $.ajax({
+        type: "POST",
+        url: "insert.php",
+        data: {
+            name:name,
+            price:price},
+        success:function(data){
+            alert(data);
+        }
+    });
+
 }
