67282b6f8794fd12c200ba54388728f8171f2c49
YerkanatSultanov <erkanatsultanov1999@gmail.com>
2022-12-19 16:20:26 +0600

    forgot dots

--- redCanteen.js
+++ redCanteen.js
@@ -1,6 +1,6 @@
 const arr = [
     {
-        html: '                <div class="samsaSKurPhoto"><img src="img/bakery%20(1).png" alt="samsa" class="photo"></div>\n' +
+        html: '                <div class="samsaSKurPhoto"><img src="./img/bakery%20(1).png" alt="samsa" class="photo"></div>\n' +
             '                <div class="text">Самса с курицей</div>\n' +
             '                <div class="priceAndCart">\n' +
             '                    <div class="price">250 Тг</div>\n' +
@@ -11,7 +11,7 @@ const arr = [
             '            </div>',
         type: 'Выпечка'
     }, {
-        html: '    <div class="samsaSKurPhoto"><img src="img/bakery%20(1).png" alt="samsa" class="photo"></div>\n' +
+        html: '    <div class="samsaSKurPhoto"><img src="./img/bakery%20(1).png" alt="samsa" class="photo"></div>\n' +
             '                <div class="text">Самса с сыром</div>\n' +
             '                <div class="priceAndCart">\n' +
             '                    <div class="price">240 Тг</div>\n' +
@@ -22,7 +22,7 @@ const arr = [
             '            </div>',
         type: 'Выпечка'
     }, {
-        html: '                <div class="samsaSKurPhoto"><img src="img/0e0ed69349edc2c963704f24e289c335.png" alt="samsa" class="photo"></div>\n' +
+        html: '                <div class="samsaSKurPhoto"><img src="./img/0e0ed69349edc2c963704f24e289c335.png" alt="samsa" class="photo"></div>\n' +
             '                <div class="text">Поча с курицой</div>\n' +
             '                <div class="priceAndCart">\n' +
             '                    <div class="price">220 Тг</div>\n' +
@@ -34,7 +34,7 @@ const arr = [
         type: 'Выпечка'
     }, {
         html:
-            '                <div class="samsaSKurPhoto"><img src="img/0e0ed69349edc2c963704f24e289c335.png" alt="samsa" class="photo"></div>\n' +
+            '                <div class="samsaSKurPhoto"><img src="./img/0e0ed69349edc2c963704f24e289c335.png" alt="samsa" class="photo"></div>\n' +
             '                <div class="text">Поча с сыром</div>\n' +
             '                <div class="priceAndCart">\n' +
             '                    <div class="price">210 Тг</div>\n' +
@@ -45,7 +45,7 @@ const arr = [
         type: 'Выпечка'
     },
     {
-        html: '                <div class="samsaSKurPhoto"><img src="img/zo2k7wmxr4i6wh9mw1quogjyxu1e8o5x.png" alt="samsa" class="photo"></div>\n' +
+        html: '                <div class="samsaSKurPhoto"><img src="./img/zo2k7wmxr4i6wh9mw1quogjyxu1e8o5x.png" alt="samsa" class="photo"></div>\n' +
             '                <div class="text">Ачма с шоколадом</div>\n' +
             '                <div class="priceAndCart">\n' +
             '                    <div class="price">250 Тг</div>\n' +
@@ -56,7 +56,7 @@ const arr = [
         type: 'Выпечка'
     },
     {
-        html: '                <div class="samsaSKurPhoto"><img src="img/29a1bd32.png" alt="samsa" class="photo"></div>\n' +
+        html: '                <div class="samsaSKurPhoto"><img src="./img/29a1bd32.png" alt="samsa" class="photo"></div>\n' +
             '                <div class="text">Круассан со сгущенкой</div>\n' +
             '                <div class="priceAndCart">\n' +
             '                    <div class="price">250 Тг</div>\n' +
@@ -67,7 +67,7 @@ const arr = [
         type: 'Десерты'
     },
     {
-        html: '                <div class="samsaSKurPhoto"><img src="img/croissant-with-chocolat.jpg" alt="samsa" class="photo"></div>\n' +
+        html: '                <div class="samsaSKurPhoto"><img src="./img/croissant-with-chocolat.jpg" alt="samsa" class="photo"></div>\n' +
             '                <div class="text">Круассан с шоколадом</div>\n' +
             '                <div class="priceAndCart">\n' +
             '                    <div class="price">250 Тг</div>\n' +
@@ -78,7 +78,7 @@ const arr = [
         type: 'Десерты'
     },
     {
-        html: '                <div class="samsaSKurPhoto"><img src="img/8747f9f6af47f3a8ff7c5b63f9dff352-680x500-removebg-preview.png" alt="samsa" class="photo" style="width: 180px; height: 180px">  </div>\n' +
+        html: '                <div class="samsaSKurPhoto"><img src="./img/8747f9f6af47f3a8ff7c5b63f9dff352-680x500-removebg-preview.png" alt="samsa" class="photo" style="width: 180px; height: 180px">  </div>\n' +
             '                <div class="text">Пиде</div>\n' +
             '                <div class="priceAndCart">\n' +
             '                    <div class="price">250 Тг</div>\n' +
@@ -89,7 +89,7 @@ const arr = [
         type: 'Выпечка'
     },
     {
-        html: '                <div class="samsaSKurPhoto"><img src="/img/a8dbaaece41cb21bcc16be71c8d79b4a.png" alt="samsa" class="photo" style="width: 180px; height: 180px">  </div>\n' +
+        html: '                <div class="samsaSKurPhoto"><img src="./img/a8dbaaece41cb21bcc16be71c8d79b4a.png" alt="samsa" class="photo" style="width: 180px; height: 180px">  </div>\n' +
             '                <div class="text">Сосиска в тесте</div>\n' +
             '                <div class="priceAndCart">\n' +
             '                    <div class="price">250 Тг</div>\n' +
@@ -99,7 +99,7 @@ const arr = [
         type: 'Выпечка'
     },
     {
-        html: '                <div class="samsaSKurPhoto"><img src="img/bcf3624ab2d24cb21fac59891a3644a4.png" alt="samsa" class="photo" style="width: 180px; height: 180px">  </div>\n' +
+        html: '                <div class="samsaSKurPhoto"><img src="./img/bcf3624ab2d24cb21fac59891a3644a4.png" alt="samsa" class="photo" style="width: 180px; height: 180px">  </div>\n' +
             '                <div class="text">Жареная сосиска в тесте</div>\n' +
             '                <div class="priceAndCart">\n' +
             '                    <div class="price">250 Тг</div>\n' +
@@ -110,7 +110,7 @@ const arr = [
         type: 'Выпечка',
     },
     {
-        html: '                <div class="samsaSKurPhoto"><img src="/img/png-transparent-chocolate-donut-with-sprinkles-illustration-doughnut-dessert-bakery-cake-donut-collection-food-cake-decorating-sweetness-removebg-preview.png" alt="samsa" class="photo" style="width: 180px;">  </div>\n' +
+        html: '                <div class="samsaSKurPhoto"><img src="./img/png-transparent-chocolate-donut-with-sprinkles-illustration-doughnut-dessert-bakery-cake-donut-collection-food-cake-decorating-sweetness-removebg-preview.png" alt="samsa" class="photo" style="width: 180px;">  </div>\n' +
             '                <div class="text">Пончик</div>\n' +
             '                <div class="priceAndCart">\n' +
             '                    <div class="price">150 Тг</div>\n' +
@@ -121,7 +121,7 @@ const arr = [
         type: 'Десерты'
     },
     {
-        html: '                <div class="samsaSKurPhoto"><img src="/img/Cheesecake-PNG-Image-HD.png" alt="samsa" class="photo">  </div>\n' +
+        html: '                <div class="samsaSKurPhoto"><img src="./img/Cheesecake-PNG-Image-HD.png" alt="samsa" class="photo">  </div>\n' +
             '                <div class="text">Чизкейк</div>\n' +
             '                <div class="priceAndCart">\n' +
             '                    <div class="price">450 Тг</div>\n' +
@@ -132,7 +132,7 @@ const arr = [
         type: 'Десерты'
     },
     {
-        html: '                <div class="samsaSKurPhoto"><img src="/img/1644441051_screenshot_24-removebg-preview.png" alt="samsa" class="photo" style=" width: 180px">  </div>\n' +
+        html: '                <div class="samsaSKurPhoto"><img src="./img/1644441051_screenshot_24-removebg-preview.png" alt="samsa" class="photo" style=" width: 180px">  </div>\n' +
             '                <div class="text">Сникерс</div>\n' +
             '                <div class="priceAndCart">\n' +
             '                    <div class="price">450 Тг</div>\n' +
@@ -143,7 +143,7 @@ const arr = [
         type: 'Десерты',
     },
     {
-        html: '               <div class="samsaSKurPhoto"><img src="https://redfish-sushi.ru/d/banana-roll-1.png" alt="samsa" class="photo">  </div>\n' +
+        html: '               <div class="samsaSKurPhoto"><img src="./img/https://redfish-sushi.ru/d/banana-roll-1.png" alt="samsa" class="photo">  </div>\n' +
             '                <div class="text">Ролл банан</div>\n' +
             '                <div class="priceAndCart">\n' +
             '                    <div class="price">350 Тг</div>\n' +
@@ -154,7 +154,7 @@ const arr = [
         type: 'Десерты'
     },
     {
-        html: '                <div class="samsaSKurPhoto"><img src="/img/fresh-bun-stuffed-with-poppy-seeds-homemade-baking_653217-361.jpg" alt="samsa" class="photo" style="width: 180px">  </div>\n' +
+        html: '                <div class="samsaSKurPhoto"><img src="./img/fresh-bun-stuffed-with-poppy-seeds-homemade-baking_653217-361.jpg" alt="samsa" class="photo" style="width: 180px">  </div>\n' +
             '                <div class="text">Булочка с маком</div>\n' +
             '                <div class="priceAndCart">\n' +
             '                    <div class="price">210 Тг</div>\n' +
@@ -165,7 +165,7 @@ const arr = [
         type: 'Десерты'
     },
     {
-        html: '                <div class="samsaSKurPhoto"><img src="/img/234-2340017_20-oz-pepsi-bottle-png-download-pepsi-caffeine-free-soda-20-fl-removebg-preview.png" alt="samsa" class="photo" style="width: 180px">  </div>\n' +
+        html: '                <div class="samsaSKurPhoto"><img src="./img/234-2340017_20-oz-pepsi-bottle-png-download-pepsi-caffeine-free-soda-20-fl-removebg-preview.png" alt="samsa" class="photo" style="width: 180px">  </div>\n' +
             '                            <div class="text">Пепси 0,5л</div>\n' +
             '                          <div class="priceAndCart">\n' +
             '                               <div class="price">360 Тг</div>\n' +
@@ -176,7 +176,7 @@ const arr = [
         type: 'Напитки'
     },
     {
-        html: '                <div class="samsaSKurPhoto"><img src="/img/hd-cold-coca-cola-plastic-bottle-png-31625599293mz441emehh-removebg-preview.png" alt="samsa" class="photo" style="width: 150px">  </div>\n' +
+        html: '                <div class="samsaSKurPhoto"><img src="./img/hd-cold-coca-cola-plastic-bottle-png-31625599293mz441emehh-removebg-preview.png" alt="samsa" class="photo" style="width: 150px">  </div>\n' +
             '                            <div class="text">Кола 0,5л</div>\n' +
             '                          <div class="priceAndCart">\n' +
             '                               <div class="price">360 Тг</div>\n' +
@@ -187,7 +187,7 @@ const arr = [
         type: 'Напитки'
     },
     {
-        html: '                <div class="samsaSKurPhoto"><img src="/img/Napitok%20Maxi%20chai%20zelenyi%20Limon%20450%20ml.png" alt="samsa" class="photo" style="width: 200px">  </div>\n' +
+        html: '                <div class="samsaSKurPhoto"><img src="./img/Napitok%20Maxi%20chai%20zelenyi%20Limon%20450%20ml.png" alt="samsa" class="photo" style="width: 200px">  </div>\n' +
             '                            <div class="text">Пепси 0,5л</div>\n' +
             '                          <div class="priceAndCart">\n' +
             '                               <div class="price">360 Тг</div>\n' +
@@ -198,7 +198,7 @@ const arr = [
         type: 'Напитки'
     },
     {
-        html: '                <div class="samsaSKurPhoto"><img src="/img/fanta_PNG51.png" alt="samsa" class="photo" style="width: 180px">  </div>\n' +
+        html: '                <div class="samsaSKurPhoto"><img src="./img/fanta_PNG51.png" alt="samsa" class="photo" style="width: 180px">  </div>\n' +
             '                            <div class="text">Пепси 0,5л</div>\n' +
             '                          <div class="priceAndCart">\n' +
             '                               <div class="price">360 Тг</div>\n' +
@@ -209,7 +209,7 @@ const arr = [
         type: 'Напитки'
     },
     {
-        html: '                <div class="samsaSKurPhoto"><img src="/img/Black-Tea-Transparent-File.png" alt="samsa" class="photo" style="width: 180px">  </div>\n' +
+        html: '                <div class="samsaSKurPhoto"><img src="./img/Black-Tea-Transparent-File.png" alt="samsa" class="photo" style="width: 180px">  </div>\n' +
             '                            <div class="text">Черный чай</div>\n' +
             '                          <div class="priceAndCart">\n' +
             '                               <div class="price">100 Тг</div>\n' +
